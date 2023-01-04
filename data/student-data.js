75087780a1955ed95ccedf2e186ee5b1b066e5a0
oscarnunez1 <requinto31@gmail.com>
2023-01-03 19:42:41 -0500

    add data

---
+++ data/student-data.js
@@ -0,0 +1,10 @@
+const pokemonTrainers = [
+  {firstName: 'Ash', lastName: 'Ketchum', age: '35', enrolled: true},
+  {firstName: 'Misty', lastName: 'Williams', age: '37', enrolled: true},
+  {firstName: 'Brock', lastName: 'Harrison', age: '40', enrolled: false},
+  {firstName: 'Gary', lastName: 'Oak', age: '35', enrolled: true},
+]
+
+export { 
+	pokemonTrainers
+}
