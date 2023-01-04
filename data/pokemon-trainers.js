71f52000c08c3b2b54c93ecd6e469a1d168c6da8
oscarnunez1 <requinto31@gmail.com>
2023-01-03 20:31:05 -0500

    fix <li>

--- data/pokemon-trainers.js
+++ data/pokemon-trainers.js
@@ -1,8 +1,8 @@
 const pokemonTrainers = [
-  {firstName: 'Ash', lastName: 'Ketchum', age: '35', status: "active"},
-  {firstName: 'Misty', lastName: 'Williams', age: '37', status: "retired"},
-  {firstName: 'Brock', lastName: 'Harrison', age: '40', status: "retired"},
-  {firstName: 'Gary', lastName: 'Oak', age: '35', status: "active"},
+  {firstName: 'Ash', lastName: 'Ketchum', age: '35', status: true,},
+  {firstName: 'Misty', lastName: 'Williams', age: '37', status: false},
+  {firstName: 'Brock', lastName: 'Harrison', age: '40', status: false},
+  {firstName: 'Gary', lastName: 'Oak', age: '35', status: true},
 ]
 
 export { 
